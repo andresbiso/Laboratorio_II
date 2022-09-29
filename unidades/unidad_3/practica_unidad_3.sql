b5cad17fa55e0da5395508a86db98e6c7d7d91eb
Andrés Biso <biso.andres@gmail.com>
2022-09-29 15:26:54 -0300

    Update practica_unidad_3.sql

--- unidades/unidad_3/practica_unidad_3.sql
+++ unidades/unidad_3/practica_unidad_3.sql
@@ -137,7 +137,35 @@ Eliminar de la tabla JOB aquella fila cuyo Job_Id es ingresado con una variable 
 
 Capturar  e informar mediante excepciones o atributos del cursor , las siguientes eventualidades: no existe el código de cargo ingresado (Sql%Notfound  o Sql%Rowcount) no puede eliminar un cargo que está asignado a empleados (Asociar una excepción con el error correspondiente) . 
 */
- 
+
+/*
+insert into job
+values (6, 'test');
+*/
+
+declare
+    v_id_cargo job.job_id%type;
+    e_cr exception;
+    pragma exception_init(e_cr, -02292);
+
+    v_ex varchar2(1);
+begin
+    v_id_cargo := :Ingrese_Id_Cargo;
+
+    select '*' into v_ex from job where job_id = v_id_cargo;
+
+    delete from job
+    where job_id = v_id_cargo;
+
+    exception
+        when no_data_found then
+            dbms_output.put_line('No existe el cargo ingresado');
+        when e_cr then
+            dbms_output.put_line('El cargo ingresado se encuentra en uso');
+        when others then
+            dbms_output.put_line('Error inesperado: ' || sqlerrm);
+end
+
 /*
 4. Escribir un bloque PL/Sql para insertar un nuevo cargo en la tabla JOB: 
 
@@ -150,6 +178,24 @@ Asentar en la base de datos este insert (Commit).
 Una vez que se ejecutó el bloque Pl/Sql  consultar desde SqlDeveloper todo el contenido de la tabla JOB. 
 */
 
+declare
+    v_max_id job.job_id%type;
+    v_cargo job.function%type;
+begin
+    select max(job_id)
+    into v_max_id
+    from job;
+
+    v_cargo := :Ingrese_Cargo;
+
+    insert into job(job_id, function)
+    values (nvl(v_max_id, 0) + 1, upper(v_cargo));
+end
+
+select * from job;
+
+-- delete from job where function = 'ESTUDIANTE';
+
 /*
 5. Escribir un bloque PL/SQL que actualice el precio de lista de un producto de acuerdo al número de veces que el producto se vendió: 
 
@@ -160,15 +206,101 @@ Calcule las veces que el producto se vendió (Tabla ITEM). Si el producto se ven
 Tenga en cuenta que el precio de lista vigente de un producto es aquel que tiene la columna END_DATE en null. 
 */
 
+declare
+    v_prod_id product.product_id%type;
+    v_cant_vendida number(4);
+    v_porcentaje number(4, 2);
+begin
+    v_prod_id := :Ingrese_Prod_id;
+
+    select count(item_id)
+    into v_cant_vendida
+    from item
+    where product_id = v_prod_id;
+
+    if v_cant_vendida in (1, 2) then
+        v_porcentaje := 1.1; 
+    elsif v_cant_vendida > 2 then
+        v_porcentaje := 1.2;
+    else
+        v_porcentaje := 1.5;
+    end if;
+
+    update price
+    set min_price = floor(min_price / v_porcentaje),
+    list_price = floor(list_price / v_porcentaje)
+    where product_id = v_prod_id and end_date is null;
+
+    exception
+        when no_data_found then
+            dbms_output.put_line('El producto ingresado no existe');
+        when others then
+            dbms_output.put_line('Error inesperado: ' || sqlerrm);
+end
+
 /*
 6. Modificar el ejercicio 5 de la práctica 2 para mostrar un mensaje en caso de no existir la orden. 
 */
 
+/*Creo que se refiere al ejercicio 4 de la práctica 2*/
+
+declare
+    v_nro_orden sales_order.order_id%type;
+    v_orden sales_order%rowtype;
+begin
+    v_nro_orden := :Ingresar_Id_orden;
+
+    select *
+    into v_orden
+    from sales_order
+    where order_id = v_nro_orden;
+
+    dbms_output.put_line(v_orden.order_id || ' ' || v_orden.order_date || ' '|| v_orden.customer_id || ' ' || v_orden.ship_date || ' ' || v_orden.total);
+    
+    exception
+        when no_data_found then
+            dbms_output.put_line('No existe la orden ingresada');
+end
+
 /*
 7. Modificar el ejercicio 8 de la práctica 2 para ingresar el apellido del empleado y mostrar su id, nombre, salario y asteriscos de acuerdo al salario. 
 Mostrar mensajes si no existe empleado con dicho apellido, o si hay más de un empleado con ese apellido.
-Salto de página
 */
+
+/*Creo que se refiere al ejercicio 9 de la práctica 2*/
+
+declare
+    v_id_emple employee.employee_id%type;
+    v_nombre_emple employee.first_name%type;
+    v_apellido_emple employee.last_name%type;
+    v_salario_emple employee.salary%type;
+    v_i number := 0;
+    v_cant_loops number := 0;
+    v_asteriscos varchar2(1000);
+begin
+    v_apellido_emple := :Ingrese_Apellido_Empleado;
+
+    select employee_id, first_name, salary
+    into v_id_emple, v_nombre_emple, v_salario_emple
+    from employee
+    where upper(last_name) = upper(v_apellido_emple);
+
+    dbms_output.put_line('El empleado ' || v_nombre_emple || ' ' || upper(v_apellido_emple) || ' con id ' || v_id_emple || ' gana ' || v_salario_emple);
+
+    v_cant_loops := floor(v_salario_emple / 100);
+    while v_i < v_cant_loops loop
+        v_asteriscos := v_asteriscos || '*';
+        v_i := v_i + 1;
+    end loop;
+
+    dbms_output.put_line(v_asteriscos || '(' || v_i || ')');
+
+    exception
+        when no_data_found then
+            dbms_output.put_line('El empleado ingresado no existe');
+        when too_many_rows then
+            dbms_output.put_line('Existe más de un empleado con ese apellido');
+end
 
 /* Segunda Parte: Cursores explícitos */
 
