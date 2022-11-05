d2846581718ae8bc3641884c729bbff4085c62fa
Andrés Biso <biso.andres@gmail.com>
2022-11-04 21:17:44 -0300

    Update practica_unidad_7.sql

--- unidades/unidad_7/practica_unidad_7.sql
+++ unidades/unidad_7/practica_unidad_7.sql
@@ -135,6 +135,171 @@ Agregar una función Valida_job que recibe el nombre del cargo y devuelve su Id,
 si no existe debe provocar una excepción que debe ser atrapada por el procedimiento que invoca a la función.
 */
 
+create or replace package PA_EMP_PACK as
+    procedure pr_alta_emp(pi_employee_id employee.employee_id%type,
+        pi_first_name employee.first_name%type,
+        pi_last_name employee.last_name%type,
+        pi_job_id job.job_id%type,
+        pi_manager_id employee.manager_id%type,
+        pi_hire_date employee.hire_date%type default sysdate);
+
+     procedure pr_alta_emp(pi_employee_id employee.employee_id%type,
+        pi_first_name employee.first_name%type,
+        pi_last_name employee.last_name%type,
+        pi_job_func job.function%type,
+        pi_manager_id employee.manager_id%type,
+        pi_hire_date employee.hire_date%type default sysdate);
+end;
+create or replace package body PA_EMP_PACK is
+    function fu_valida_job (pi_job_id job.job_id%type)
+        return boolean
+    is
+        l_job_id job.job_id%type;
+
+        begin
+            select job_id
+            into l_job_id
+            from job
+            where job_id = pi_job_id;
+
+            return true;
+
+            exception
+                when no_data_found then
+                    return false;
+                when others then
+                    return false;
+        end;
+
+    function fu_valida_job (pi_job_func job.function%type)
+        return job.job_id%type
+    is
+        l_job_id job.job_id%type;
+
+        begin
+            select job_id
+            into l_job_id
+            from job
+            where upper(function) = upper(pi_job_func);
+
+            return l_job_id;
+
+            exception
+                when no_data_found then
+                    raise_application_error(-20001, 'No existe el cargo');
+                when too_many_rows then
+                    raise_application_error(-20002, 'Existe más de un cargo con ese nombre');
+                when others then
+                    raise_application_error(-20003, 'Error inesperado: ' || sqlerrm);
+        end;
+
+    function fu_valida_jefe (pi_manager_id employee.manager_id%type)
+        return boolean
+    is
+        l_manager_id number := 0;
+
+        begin
+            select count(employee_id)
+            into l_manager_id
+            from employee
+            where manager_id = pi_manager_id;
+
+            return l_manager_id > 0;
+
+            exception
+                when no_data_found then
+                    return false;
+                when others then
+                    return false;
+        end;
+
+    procedure pr_alta_emp(pi_employee_id employee.employee_id%type,
+        pi_first_name employee.first_name%type,
+        pi_last_name employee.last_name%type,
+        pi_job_id job.job_id%type,
+        pi_manager_id employee.manager_id%type,
+        pi_hire_date employee.hire_date%type default sysdate)
+    as
+        emplo_dept employee.department_id%type;
+        emplo_salary employee.salary%type;
+
+        e_job_noex exception;
+        e_manager_noex exception;
+        pragma exception_init(e_job_noex, -20001);
+        pragma exception_init(e_manager_noex, -20002);
+    begin
+        if not fu_valida_job(pi_job_id) then
+            raise_application_error(-20001, 'El job ingresado no existe');
+        end if;
+        if not fu_valida_jefe(pi_manager_id) then
+             raise_application_error(-20002, 'El jefe ingresado no existe');
+        end if;
+
+        select department_id
+        into emplo_dept
+        from employee
+        where employee_id = pi_manager_id;
+
+        select min(salary)
+        into emplo_salary
+        from employee
+        where job_id = pi_job_id and department_id = emplo_dept;
+
+        insert into employee (employee_id, last_name, first_name, middle_initial, job_id, manager_id, hire_date, salary, commission, department_id)
+        values (pi_employee_id, pi_last_name, pi_first_name, null, pi_job_id, pi_manager_id, pi_hire_date, emplo_salary, null, emplo_dept);
+        
+         dbms_output.put_line('El empleado fue creado con éxito');
+
+        exception
+            when e_job_noex then
+                dbms_output.put_line('El job ingresado no existe');
+            when e_manager_noex then
+                dbms_output.put_line('El jefe ingresado no existe');
+            when dup_val_on_index then
+                dbms_output.put_line('El empleado ya existe');
+            when others then
+                dbms_output.put_line('Error inesperado: ' || sqlerrm);
+    end pr_alta_emp;
+
+    procedure pr_alta_emp(pi_employee_id employee.employee_id%type,
+        pi_first_name employee.first_name%type,
+        pi_last_name employee.last_name%type,
+        pi_job_func job.function%type,
+        pi_manager_id employee.manager_id%type,
+        pi_hire_date employee.hire_date%type default sysdate)
+    as
+        emplo_dept employee.department_id%type;
+        emplo_salary employee.salary%type;
+        job_id job.job_id%type;
+
+        e_no_data exception;
+        e_too_many exception;
+        e_other exception;
+        pragma exception_init(e_no_data, -020001);
+        pragma exception_init(e_too_many, -020002);
+        pragma exception_init(e_other, -020003);
+    begin
+        job_id := fu_valida_job(pi_job_func);
+
+        pr_alta_emp(pi_employee_id, pi_first_name, pi_last_name, job_id, pi_manager_id, pi_hire_date);
+
+        exception
+            when e_no_data then
+                dbms_output.put_line('No existe el cargo');
+            when e_too_many then
+                dbms_output.put_line('Existe m{as de un cargo con ese nombre');
+            when e_other then
+                dbms_output.put_line('Error inesperado: ' || sqlerrm);
+    end pr_alta_emp;
+end PA_EMP_PACK;
+
+begin
+    PA_EMP_PACK.pr_alta_emp(56,'john','diaz','staff',7902);
+end
+
+-- select * from employee where employee_id = 56
+-- delete from employee where employee_id = 56
+
 /*
 3. Crear un paquete ABM_JOB para actualizar cargos:
 
